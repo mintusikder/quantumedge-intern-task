a48b60189ac6c42edbdf038347cc29df92c5239a
Mintu Sikder <mintusikder15@gmail.com>
2025-07-15 00:00:11 +0600

    added title

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Header from '../pages/Header';
-import { Outlet } from 'react-router';
-import Footer from '../pages/Footer';
+import React from "react";
+import Header from "../pages/Header";
+import { Outlet } from "react-router";
+import Footer from "../pages/Footer";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <div className="min-h-[calc(100vh-80px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
